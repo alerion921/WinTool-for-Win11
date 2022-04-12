e121bcaafc317066b7107b1476b4991ba89b6d38
alerion921 <61840538+alerion921@users.noreply.github.com>
2022-04-12 15:38:51 +0200

    Update WinTool.ps1

--- WinTool.ps1
+++ WinTool.ps1
@@ -2026,13 +2026,13 @@ $gimp.Add_Click({
 
 $windows10ify11.Add_Click({
     # Restore the Classic Taskbar in Windows 11
-    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell\Update\Packages" -Name "UndockingDisabled" -Type "DWord" -Value "1";
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell\Update\Packages" -Name "UndockingDisabled" -Type "DWord" -Value "1"
 
     # Ungroup Taskbar Icons / Enable Text Labels in Windows 11
-    New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoTaskGrouping" -Type "DWord" -Value "1";
+    New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoTaskGrouping" -Type "DWord" -Value "1"
 
     # Windows 10 Start Menu
-    New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_ShowClassicMode" -Type "DWord" -Value "1";
+    New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_ShowClassicMode" -Type "DWord" -Value "1"
 
     Stop-Process -name explorer
     Start-Sleep -s 5
@@ -2040,9 +2040,9 @@ $windows10ify11.Add_Click({
 })
 
 $snapbackto11.Add_Click({
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell\Update\Packages" -Name "UndockingDisabled" -Type "DWord" -Value "0"
-    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoTaskGrouping" -Type "DWord" -Value "0";
-    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_ShowClassicMode" -Type "DWord" -Value "0";
+    Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell\Update\Packages" -Name "UndockingDisabled"
+    Remove-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoTaskGrouping"
+    Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_ShowClassicMode"
     
     Stop-Process -name explorer
     Start-Sleep -s 5
