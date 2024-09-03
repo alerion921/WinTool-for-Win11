04c37b111e538db87b4298822d4ce31e39d9e289
alerion921 <61840538+alerion921@users.noreply.github.com>
2024-09-03 12:41:19 +0200

    Onedrive removal updates
    Script taken from winutil by chis and some improvements added to remove it when installed with Chocolatey

--- WinTool.ps1
+++ WinTool.ps1
@@ -1,6 +1,6 @@
-$HidePowershellWindow = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
-add-type -name win -member $HidePowershellWindow -namespace native
-[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
+#$HidePowershellWindow = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
+#add-type -name win -member $HidePowershellWindow -namespace native
+#[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
 
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
@@ -20,6 +20,7 @@ $pathDocuments      = [Environment]::GetFolderPath("MyDocuments")
 #$pathAppdataRoaming = [Environment]::GetFolderPath("ApplicationData")
 #$pathWindows        = [Environment]::GetFolderPath("Windows")
 #$pathSystem         = [Environment]::GetFolderPath("System")
+
 ####################################################################################
 Function MakeForm {
 
@@ -221,19 +222,33 @@ Function MakeForm {
     $securitypatches.FlatStyle = "Flat"
     $securitypatches.FlatAppearance.MouseOverBackColor = $hovercolor
 
-    $onedrive = New-Object system.Windows.Forms.Button
-    $onedrive.text = "Remove OneDrive"
-    $onedrive.width = 220
-    $onedrive.height = 30
-    $onedrive.location = New-Object System.Drawing.Point(0, 325)
-    $onedrive.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 12)
-    $onedrive.BackColor = $frontcolor 
-    $onedrive.ForeColor = $backcolor
-    $onedrive.FlatStyle = "Flat"
-    $onedrive.FlatAppearance.MouseOverBackColor = $hovercolor
+    if(Test-Path "$env:programdata\Microsoft OneDrive", "C:\Program Files (x86)\Microsoft OneDrive", "C:\Program Files\Microsoft OneDrive") {
+        $onedrive = New-Object system.Windows.Forms.Button
+        $onedrive.text = "Remove OneDrive"
+        $onedrive.width = 220
+        $onedrive.height = 30
+        $onedrive.location = New-Object System.Drawing.Point(0, 325)
+        $onedrive.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 12)
+        $onedrive.BackColor = $frontcolor 
+        $onedrive.ForeColor = $backcolor
+        $onedrive.FlatStyle = "Flat"
+        $onedrive.FlatAppearance.MouseOverBackColor = $hovercolor
+    }
+    else {
+        $onedrive = New-Object system.Windows.Forms.Button
+        $onedrive.text = "Restore OneDrive"
+        $onedrive.width = 220
+        $onedrive.height = 30
+        $onedrive.location = New-Object System.Drawing.Point(0, 325)
+        $onedrive.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 12)
+        $onedrive.BackColor = $frontcolor 
+        $onedrive.ForeColor = $backcolor
+        $onedrive.FlatStyle = "Flat"
+        $onedrive.FlatAppearance.MouseOverBackColor = $hovercolor 
+    }
 
     $InstallOneDrive = New-Object system.Windows.Forms.Button
-    $InstallOneDrive.text = "Restore OneDrive"
+    $InstallOneDrive.text = "Something else"
     $InstallOneDrive.width = 220
     $InstallOneDrive.height = 30
     $InstallOneDrive.location = New-Object System.Drawing.Point(0, 360)
@@ -3177,35 +3192,119 @@ Function MakeForm {
         })
 
     $onedrive.Add_Click({
-            $Form.text = "WinTool by Alerion - Removing OneDrive..."
-            $ResultText.text = " Disabling OneDrive..."
-            If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive")) {
-                New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive" | Out-Null
+        if(Test-Path "$env:programdata\Microsoft OneDrive", "C:\Program Files (x86)\Microsoft OneDrive", "C:\Program Files\Microsoft OneDrive") {
+                $Form.text = "WinTool by Alerion - Removing OneDrive..."
+                $ResultText.text = " Onedrive is being uninstalled..."
+                $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\OneDriveSetup.exe"
+                if (Test-Path $regPath) {
+                    $OneDriveUninstallString = Get-ItemPropertyValue "$regPath" -Name "UninstallString"
+                    $OneDriveExe, $OneDriveArgs = $OneDriveUninstallString.Split(" ")
+                    Start-Process -FilePath $OneDriveExe -ArgumentList "$OneDriveArgs /silent" -NoNewWindow -Wait
+                } 
+                else {
+                    
+                    "%programdata%\chocolatey\lib\onedrive\tools\chocolateyuninstall.ps1"
+                    if(Test-Path "C:\Program Files\Microsoft OneDrive", "C:\Program Files (x86)\Microsoft OneDrive") {
+                        taskkill.exe /F /IM "OneDrive.exe"
+                        Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "C:\Program Files\Microsoft OneDrive"
+                        Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "C:\Program Files (x86)\Microsoft OneDrive"
+        
+                        taskkill.exe /F /IM "explorer.exe"
+                        Start-Process "explorer.exe"
+                    }
+
+                }
+
+                # Check if OneDrive got Uninstalled
+                if (-not (Test-Path $regPath) -or (Test-Path "C:\Program Files\Microsoft OneDrive", "C:\Program Files (x86)\Microsoft OneDrive")) {
+                    $Form.text = "WinTool by Alerion - Backing up user created files..."
+                    $ResultText.text = " User created files are being backed up and moved to the root of the user folder..." # Change this to the user desktop instead $env:USERPROFILE.TrimEnd()
+                    Start-Process -FilePath powershell -ArgumentList "robocopy '$($OneDrivePath)' '$("$env:USERPROFILE\Desktop")\' /mov /e /xj" -NoNewWindow -Wait
+            
+                    taskkill.exe /F /IM "explorer.exe"
+                    
+                    cmd /c del C:\Program Files\Microsoft OneDrive
+
+                    #Disable FileSynch to be able to delete the remaining files later
+                    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive" -Name "DisableFileSyncNGSC" -Type DWord -Value 1            
+
+                    $Form.text = "WinTool by Alerion - Removing OneDrive leftovers..."
+                    $ResultText.text = " Removing OneDrive leftovers..."
+                    Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$env:localappdata\Microsoft\OneDrive"
+                    Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$env:localappdata\OneDrive"
+                    Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$env:programdata\Microsoft OneDrive"
+                    Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$env:systemdrive\OneDriveTemp"
+                    Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "C:\Program Files\Microsoft OneDrive"
+                    Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "C:\Program Files (x86)\Microsoft OneDrive"
+
+                    reg delete "HKEY_CURRENT_USER\Software\Microsoft\OneDrive" -f
+                    # check if directory is empty before removing:
+                    If ((Get-ChildItem "$OneDrivePath" -Recurse | Measure-Object).Count -eq 0) {
+                        Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$OneDrivePath"
+                    }
+            
+                    $Form.text = "WinTool by Alerion - Remove Onedrive from explorer sidebar..."
+                    $ResultText.text = " Remove Onedrive from explorer sidebar..."
+                    Set-ItemProperty -Path "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Name "System.IsPinnedToNameSpaceTree" -Value 0
+                    Set-ItemProperty -Path "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Name "System.IsPinnedToNameSpaceTree" -Value 0
+            
+                    $Form.text = "WinTool by Alerion - Removing run hook for new users..."
+                    $ResultText.text = " Removing run hook for new users..."
+                    reg load "hku\Default" "C:\Users\Default\NTUSER.DAT"
+                    reg delete "HKEY_USERS\Default\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "OneDriveSetup" /f
+                    reg unload "hku\Default"
+            
+                    $Form.text = "WinTool by Alerion - Removing startmenu entry..."
+                    $ResultText.text = " Removing startmenu entry..."
+                    Remove-Item -Force -ErrorAction SilentlyContinue "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk"
+            
+                    $Form.text = "WinTool by Alerion - Removing scheduled task..."
+                    $ResultText.text = " Removing scheduled task..."
+                    Get-ScheduledTask -TaskPath '\' -TaskName 'OneDrive*' -ea SilentlyContinue | Unregister-ScheduledTask -Confirm:$false
+
+                    # Restoring userfolders to default locations
+                    $Form.text = "WinTool by Alerion - Restoring userfolders to default locations..."
+                    $ResultText.text = " Restoring userfolders to default locations..."
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "AppData" -Value "$env:userprofile\AppData\Roaming" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "Cache" -Value "$env:userprofile\AppData\Local\Microsoft\Windows\INetCache" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "Cookies" -Value "$env:userprofile\AppData\Local\Microsoft\Windows\INetCookies" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "Favorites" -Value "$env:userprofile\Favorites" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "History" -Value "$env:userprofile\AppData\Local\Microsoft\Windows\History" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "Local AppData" -Value "$env:userprofile\AppData\Local" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "My Music" -Value "$env:userprofile\Music" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "My Video" -Value "$env:userprofile\Videos" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "NetHood" -Value "$env:userprofile\AppData\Roaming\Microsoft\Windows\Network Shortcuts" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "PrintHood" -Value "$env:userprofile\AppData\Roaming\Microsoft\Windows\Printer Shortcuts" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "Programs" -Value "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "Recent" -Value "$env:userprofile\AppData\Roaming\Microsoft\Windows\Recent" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "SendTo" -Value "$env:userprofile\AppData\Roaming\Microsoft\Windows\SendTo" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "Start Menu" -Value "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "Startup" -Value "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "Templates" -Value "$env:userprofile\AppData\Roaming\Microsoft\Windows\Templates" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}" -Value "$env:userprofile\Downloads" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "Desktop" -Value "$env:userprofile\Desktop" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "My Pictures" -Value "$env:userprofile\Pictures" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "Personal" -Value "$env:userprofile\Documents" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{F42EE2D3-909F-4907-8871-4C22FC0BF756}" -Value "$env:userprofile\Documents" -Type ExpandString
+                    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{0DDD015D-B06C-45D5-8C4C-F59713854639}" -Value "$env:userprofile\Pictures" -Type ExpandString
+
+                    Start-Process "explorer.exe"
+            
+                    $Form.text = "WinTool by Alerion - Onedrive has been successfully removed..."
+                    Start-Sleep 5
+                } else {
+                    $ResultText.text = " Something went Wrong during the Unistallation of OneDrive. `r`n Ready for Next Task!"
+                }
+                
+                $ResultText.text = " Deleted and Disabled OneDrive. `r`n Ready for Next Task!"
             }
-            Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive" -Name "DisableFileSyncNGSC" -Type DWord -Value 1
-            $ResultText.text = " Uninstalling OneDrive..."
-            Stop-Process -Name "OneDrive" -ErrorAction SilentlyContinue
-            Start-Sleep -s 2
-            $onedrive = "$env:SYSTEMROOT\SysWOW64\OneDriveSetup.exe"
-            If (!(Test-Path $onedrive)) {
-                $onedrive = "$env:SYSTEMROOT\System32\OneDriveSetup.exe"
+            else {
+                $Form.text = "WinTool by Alerion - Reinstalling OneDrive..."
+                $ResultText.text = " Installing Onedrive. Please Wait..."
+                Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive" -Name "DisableFileSyncNGSC" -ErrorAction SilentlyContinue
+                choco install onedrive -y --force
+                $ResultText.text = " Finished Reinstalling OneDrive. `r`n Ready for Next Task!"
             }
-            Start-Process $onedrive "/uninstall" -NoNewWindow -Wait
-            Start-Sleep -s 2
-            Stop-Process -Name "explorer" -ErrorAction SilentlyContinue
-            Start-Sleep -s 2
-            $ResultText.text = " Removing extra OneDrive leftovers..."
-            Remove-Item -Path "$env:USERPROFILE\OneDrive" -Force -Recurse -ErrorAction SilentlyContinue
-            Remove-Item -Path "$env:LOCALAPPDATA\Microsoft\OneDrive" -Force -Recurse -ErrorAction SilentlyContinue
-            Remove-Item -Path "$env:PROGRAMDATA\Microsoft OneDrive" -Force -Recurse -ErrorAction SilentlyContinue
-            Remove-Item -Path "$env:SYSTEMDRIVE\OneDriveTemp" -Force -Recurse -ErrorAction SilentlyContinue
-            If (!(Test-Path "HKCR:")) {
-                New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT | Out-Null
-            }
-            Remove-Item -Path "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Recurse -ErrorAction SilentlyContinue
-            Remove-Item -Path "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Recurse -ErrorAction SilentlyContinue
-            $ResultText.text = " Deleted and Disabled OneDrive. `r`n Ready for Next Task!"
-            $Form.text = "WinTool by Alerion"
         })
 
     $darkmode.Add_Click({
@@ -3223,12 +3322,7 @@ Function MakeForm {
         })
 
     $InstallOneDrive.Add_Click({
-            $Form.text = "WinTool by Alerion - Reinstalling OneDrive..."
-            $ResultText.text = " Installing Onedrive. Please Wait..."
-            Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive" -Name "DisableFileSyncNGSC" -ErrorAction SilentlyContinue
-            %systemroot%\SysWOW64\OneDriveSetup.exe
-            $ResultText.text = " Finished Reinstalling OneDrive. `r`n Ready for Next Task!"
-            $Form.text = "WinTool by Alerion"
+            
         })
 
     $DisableNumLock.Add_Click({
